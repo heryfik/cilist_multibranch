8194c1375ab405b2f4294dd2d2e7ccb9fc161c4d
heryfik <hery.fik@gmail.com>
2023-01-30 15:29:38 +0700

    Update UserList.js

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -33,7 +33,7 @@ const UserList = () => {
   return (
     <div className="columns mt-5 is-centered">
       <div className="column is-half">
-      <Banner title="STAGING Users List" />
+      <Banner title="STAGING 22 Users List" />
         <Link to={`add`} className="button is-info">
           Add New
         </Link>
